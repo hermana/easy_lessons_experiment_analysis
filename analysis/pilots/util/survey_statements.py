9876c4476e850e8835b51c2859772754d17996ef
hermana <aeherman@ualberta.ca>
2026-09-07 14:20:21 -0600

    latest analysis

---
+++ analysis/pilots/util/survey_statements.py
@@ -0,0 +1,15 @@
+MOTIVATIONAL_SURVEY_STATEMENTS = {
+    'imi_interest_enjoyment': "I think this activity is enjoyable.",
+    'imi_perceived_competence': "I am satisfied with my performance in this activity.",
+    'imi_value_usefulness': "I think this activity could help me to learn the locations of letters on a keyboard.",
+    'pens_competence': "My ability to do this activity is well matched with how challenging it is.",
+    'pens_tension': "This activity is frustrating.",
+    'imi_effort_importance': "I put a lot of effort into this activity.",
+    'pens_presence_immersion': "When I found a letter I was looking for, I felt a sense of accomplishment.",
+}
+
+
+
+
+
+
